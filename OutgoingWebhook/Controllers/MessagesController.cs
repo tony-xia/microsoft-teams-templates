d283a28def6557374b1dc38c88c453099fa69ef6
Tony Xia <tonyxia@yipinapp.com>
2018-03-14 23:57:54 +1100

    refactoring the directory structure

---
+++ OutgoingWebhook/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicrosoftTeams.Templates.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET api/values
+        [HttpPost]
+        [Route("api/messages")]
+        public IActionResult ReplyMessage()
+        {
+            return Ok("{\"text\":\"reply message\"}");
+        }
+
+    }
+}
