d72380a20dc89c0f31a5f819a1308a518202e680
cl <liufen_tj@hotmail.com>
2020-10-10 18:02:26 +1100

    Removed unused property and methods

--- src/MessagingExtension/Startup.cs
+++ src/MessagingExtension/Startup.cs
@@ -8,13 +8,6 @@ namespace MicrosoftTeams.MessagingExtension
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
