2b63c1975588d42f11708129301ccc0d5d5fe665
Crystal <liufen_tj@hotmail.com>
2022-10-01 21:56:38 +1000

    Updated "OutgoingWebhook" template to use file scoped namespaces

--- src/OutgoingWebhook/Services/TeamsAuthProvider.cs
+++ src/OutgoingWebhook/Services/TeamsAuthProvider.cs
@@ -6,82 +6,81 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MicrosoftTeams.OutgoingWebhook.Services
-{
-    /// <summary>
-    /// Provides authentication results.
-    /// </summary>
-    public class TeamsAuthProvider : ITeamsAuthProvider
-    {
-        private readonly ILogger<TeamsAuthProvider> _logger;
-        private readonly string _securityToken;
+namespace MicrosoftTeams.OutgoingWebhook.Services;
 
-        public TeamsAuthProvider(ILogger<TeamsAuthProvider> logger, IConfiguration configuration)
+/// <summary>
+/// Provides authentication results.
+/// </summary>
+public class TeamsAuthProvider : ITeamsAuthProvider
+{
+    private readonly ILogger<TeamsAuthProvider> _logger;
+    private readonly string _securityToken;
+
+    public TeamsAuthProvider(ILogger<TeamsAuthProvider> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _securityToken = configuration.GetValue<string>("TeamsAppSecurityToken");
+    }
+
+    /// <summary>
+    /// Validates the specified authentication header value.
+    /// </summary>
+    /// <param name="request">The HTTP request message.</param>
+    /// <returns>
+    /// Response containing result of validation.
+    /// </returns>
+    public TeamsAuthResponse Validate(HttpRequest request)
+    {
+        request.Body.Seek(0, SeekOrigin.Begin);
+        string messageContent = new StreamReader(request.Body).ReadToEnd();
+        var authenticationHeaderValue = request.Headers["Authorization"];
+
+        if (authenticationHeaderValue.Count <= 0)
         {
-            _logger = logger;
-            _securityToken = configuration.GetValue<string>("TeamsAppSecurityToken");
+            return new TeamsAuthResponse(false, "Authentication header not present on request.");
         }
 
-        /// <summary>
-        /// Validates the specified authentication header value.
-        /// </summary>
-        /// <param name="request">The HTTP request message.</param>
-        /// <returns>
-        /// Response containing result of validation.
-        /// </returns>
-        public TeamsAuthResponse Validate(HttpRequest request)
+        if (!authenticationHeaderValue[0].StartsWith("HMAC"))
         {
-            request.Body.Seek(0, SeekOrigin.Begin);
-            string messageContent = new StreamReader(request.Body).ReadToEnd();
-            var authenticationHeaderValue = request.Headers["Authorization"];
+            return new TeamsAuthResponse(false, "Incorrect authorization header scheme.");
+        }
 
-            if (authenticationHeaderValue.Count <= 0)
+        // Reject all empty messages
+        if (string.IsNullOrEmpty(messageContent))
+        {
+            return new TeamsAuthResponse(false, "Unable to validate authentication header for messages with empty body.");
+        }
+
+        string providedHmacValue = authenticationHeaderValue[0].Substring("HMAC".Length).Trim();
+        string calculatedHmacValue = null;
+        try
+        {
+            byte[] serializedPayloadBytes = Encoding.UTF8.GetBytes(messageContent);
+
+            byte[] keyBytes = Convert.FromBase64String(_securityToken);
+            using (HMACSHA256 hmacSHA256 = new HMACSHA256(keyBytes))
             {
-                return new TeamsAuthResponse(false, "Authentication header not present on request.");
+                byte[] hashBytes = hmacSHA256.ComputeHash(serializedPayloadBytes);
+                calculatedHmacValue = Convert.ToBase64String(hashBytes);
             }
 
-            if (!authenticationHeaderValue[0].StartsWith("HMAC"))
+            if (string.Equals(providedHmacValue, calculatedHmacValue))
             {
-                return new TeamsAuthResponse(false, "Incorrect authorization header scheme.");
+                return new TeamsAuthResponse(true, null);
             }
-
-            // Reject all empty messages
-            if (string.IsNullOrEmpty(messageContent))
+            else
             {
-                return new TeamsAuthResponse(false, "Unable to validate authentication header for messages with empty body.");
+                string errorMessage = string.Format(
+                    "AuthHeaderValueMismatch. Expected:'{0}' Provided:'{1}'",
+                    calculatedHmacValue,
+                    providedHmacValue);
+                return new TeamsAuthResponse(false, errorMessage);
             }
-
-            string providedHmacValue = authenticationHeaderValue[0].Substring("HMAC".Length).Trim();
-            string calculatedHmacValue = null;
-            try
-            {
-                byte[] serializedPayloadBytes = Encoding.UTF8.GetBytes(messageContent);
-
-                byte[] keyBytes = Convert.FromBase64String(_securityToken);
-                using (HMACSHA256 hmacSHA256 = new HMACSHA256(keyBytes))
-                {
-                    byte[] hashBytes = hmacSHA256.ComputeHash(serializedPayloadBytes);
-                    calculatedHmacValue = Convert.ToBase64String(hashBytes);
-                }
-
-                if (string.Equals(providedHmacValue, calculatedHmacValue))
-                {
-                    return new TeamsAuthResponse(true, null);
-                }
-                else
-                {
-                    string errorMessage = string.Format(
-                        "AuthHeaderValueMismatch. Expected:'{0}' Provided:'{1}'",
-                        calculatedHmacValue,
-                        providedHmacValue);
-                    return new TeamsAuthResponse(false, errorMessage);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogWarning(ex, "Exception occured while verifying HMAC on the incoming request.");
-                return new TeamsAuthResponse(false, "Exception thrown while verifying MAC on incoming request.");
-            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning(ex, "Exception occured while verifying HMAC on the incoming request.");
+            return new TeamsAuthResponse(false, "Exception thrown while verifying MAC on incoming request.");
         }
     }
 }
--- src/OutgoingWebhook/Services/TeamsAuthResponse.cs
+++ src/OutgoingWebhook/Services/TeamsAuthResponse.cs
@@ -1,35 +1,34 @@
-namespace MicrosoftTeams.OutgoingWebhook.Services
+namespace MicrosoftTeams.OutgoingWebhook.Services;
+
+/// <summary>
+/// Encapsulates auth results.
+/// </summary>
+public class TeamsAuthResponse
 {
     /// <summary>
-    /// Encapsulates auth results.
+    /// Initializes a new instance of the <see cref="TeamsAuthResponse"/> class.
     /// </summary>
-    public class TeamsAuthResponse
+    /// <param name="authSuccessful">if set to <c>true</c> then [authentication was successful].</param>
+    /// <param name="errorMessage">The error message.</param>
+    public TeamsAuthResponse(bool authSuccessful, string errorMessage)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TeamsAuthResponse"/> class.
-        /// </summary>
-        /// <param name="authSuccessful">if set to <c>true</c> then [authentication was successful].</param>
-        /// <param name="errorMessage">The error message.</param>
-        public TeamsAuthResponse(bool authSuccessful, string errorMessage)
-        {
-            this.AuthSuccessful = authSuccessful;
-            this.ErrorMessage = errorMessage;
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether [authentication successful].
-        /// </summary>
-        /// <value>
-        /// <c>true</c> if [authentication successful]; otherwise, <c>false</c>.
-        /// </value>
-        public bool AuthSuccessful { get; private set; }
-
-        /// <summary>
-        /// Gets the error message.
-        /// </summary>
-        /// <value>
-        /// The error message.
-        /// </value>
-        public string ErrorMessage { get; private set; }
+        this.AuthSuccessful = authSuccessful;
+        this.ErrorMessage = errorMessage;
     }
+
+    /// <summary>
+    /// Gets a value indicating whether [authentication successful].
+    /// </summary>
+    /// <value>
+    /// <c>true</c> if [authentication successful]; otherwise, <c>false</c>.
+    /// </value>
+    public bool AuthSuccessful { get; private set; }
+
+    /// <summary>
+    /// Gets the error message.
+    /// </summary>
+    /// <value>
+    /// The error message.
+    /// </value>
+    public string ErrorMessage { get; private set; }
 }
